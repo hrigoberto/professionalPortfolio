96e22cd6862e1c130f4a21b74f2a49455a40867f
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-26 18:22:25 -0400

    fixed linter errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
-var mailer = require('./mailer.js')
+var mailer = require('./mailer.js');
 
 var port = process.env.PORT || 8080;
-var apikey = process.env.API || require('./config').apiKey
+var apikey = process.env.API || require('./config').apiKey;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
