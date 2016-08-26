d93debac37543656167cd96bb5b7364d5be1a5d2
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-26 19:01:14 -0400

    added heroku url for mailer

--- public/js/controllers/MailController.js
+++ public/js/controllers/MailController.js
@@ -7,13 +7,13 @@
   function MailController($scope, $http){
     $scope.sendMail = sendMail;
     // $scope.msg = msg;
-    var baseUrl = 'http://localhost:8080/send'
+    var baseUrl = 'https://rh3portfolio.herokuapp.com/#/send';
 
     function sendMail(msg){
       return $http.post(baseUrl, msg)
                   .then(function(response){
                     console.log('sent email', response);
-                  })
+                  });
     }
   }
 }());
